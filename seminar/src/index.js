5ef18825bb2ae5c4ea443006f312099b2fd1d924
Qndndn <hyojin0510@kaist.ac.kr>
2023-05-04 01:31:19 +0900

    HW1-EditingDB

--- seminar/src/index.js
+++ seminar/src/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-require('dotenv').config({path: '.env.example'});
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
 
 const statusRouter = require('./routes/status');
 const feedRouter = require('./routes/feed');
@@ -34,11 +37,11 @@ app.use('/ssr', ssrRouter);
 app.use('/static', express.static(path.join(__dirname,'public')));
 
 // Connect to MongoDB
-// const OMongooseOption = { useNewUrlParser: true, useUnifiedTopology: true };
-// mongoose.connect(process.env.MONGO_URI, OMongooseOption).then(
-//     () => { console.log("[Mongoose] Connection Complete!") },
-//     (err) => { console.log(`[Mongoose] Connection Error: ${ err }`) }
-// );
+const OMongooseOption = { useNewUrlParser: true, useUnifiedTopology: true };
+mongoose.connect(process.env.MONGO_URI, OMongooseOption).then(
+    () => { console.log("[Mongoose] Connection Complete!") },
+    (err) => { console.log(`[Mongoose] Connection Error: ${ err }`) }
+);
 
 app.listen(port, () => {
    console.log(`Example App Listening @ http://localhost:${ port }`);
